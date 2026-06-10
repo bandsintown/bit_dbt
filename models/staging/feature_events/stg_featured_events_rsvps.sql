e31fc17cc88e0ea9497a4a350f76f9fd26186299
vidagharavian <vgharavian@bandsintown.com>
2026-06-10 08:28:10 -0400

    Fix fe_source casting in RSVP SQL model
    Corrects the casting of the fe_source field to ensure proper data type handling. This change improves data integrity and consistency in the SQL model for featured event RSVPs.

--- models/staging/feature_events/stg_featured_events_rsvps.sql
+++ models/staging/feature_events/stg_featured_events_rsvps.sql
@@ -8,12 +8,11 @@
 select
     cast(cast(artist_event_int_id as varchar) as integer) as artist_event_int_id,
     cast(substr(cast(ds as varchar), 1, 10) as date) as ds,
-    cast(source as varchar) as fe_source,
+    cast(fe_source as varchar) as fe_source,
     cast(nonce as varchar) as nonce,
     cast(cast(user_id as varchar) as integer) as user_id,
     cast(status as varchar) as status,
     cast(action as varchar) as action,
     cast(replace(cast(timestamp as varchar), 'T', ' ') as timestamp) as rsvp_timestamp,
-    cast(replace(cast(created_at as varchar), 'T', ' ') as timestamp) as created_at,
-    fe_sources
+    cast(replace(cast(created_at as varchar), 'T', ' ') as timestamp) as created_at
 from {{ source('featured_events', 'rsvps') }}
